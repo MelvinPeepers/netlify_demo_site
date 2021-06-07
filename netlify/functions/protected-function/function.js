169bc280b4225c169047c0fd0ba8fbfb804ab8f0
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2021-06-07 11:14:03 -0400

    trying out random work function

--- netlify/functions/protected-function/function.js
+++ netlify/functions/protected-function/function.js
@@ -1,10 +1,23 @@
-exports.handler = function (event, context, callback) {
 
-  console.log("got a request! I'm sending back the request headers now")
-  console.log(event.headers)
-
+exports.handler = function(event, context, callback) {
+  const randomWords = [
+    'copper',
+    'explain',
+    'tenuous',
+    'neat',
+    'discovery',
+    'sweltering',
+    'dusty',
+    'unruly',
+    'week',
+    'rejoice',
+  ];
+  const randomNumber = Math.floor(Math.random() * 10);
   callback(null, {
-      statusCode: 200,
-      body: JSON.stringify(event.headers)
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: JSON.stringify({ word: randomWords[randomNumber] }),
   });
-}
+};
